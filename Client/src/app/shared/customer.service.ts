906c6bf58f7fa02eac2ac8f53316502a1accc3b4
AliCan1033 <turk.can.1071@gmail.com>
2021-08-13 15:20:31 +0300

    CustomerService (getCustomerList) OrderComponent(updateGrandTotal validateForm)

--- Client/src/app/shared/customer.service.ts
+++ Client/src/app/shared/customer.service.ts
@@ -1,9 +1,17 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CustomerService {
 
-  constructor() { }
+  apiUrl = environment.apiUrl
+
+  constructor(private http: HttpClient) { }
+
+  getCustomerList(){
+    return this.http.get(this.apiUrl + '/Customers').toPromise();
+  }
 }
